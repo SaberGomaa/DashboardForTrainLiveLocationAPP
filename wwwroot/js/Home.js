88569dc7e873e691ab2a786889ccf0ab366a67d7
sabegomaa261 <sabergomaa261@gmail.com>
2023-05-09 17:43:39 +0300

    Hide Text

--- wwwroot/js/Home.js
+++ wwwroot/js/Home.js
@@ -1,7 +1,26 @@
-﻿let menuicn = document.querySelector(".menuicn");
+﻿
+const hideButton = document.getElementById('menuicn');
+const spans = document.querySelectorAll('#allspan span');
+
+let isHidden = false;
+
+
+let menuicn = document.querySelector(".menuicn");
 let nav = document.querySelector(".navcontainer");
 
 menuicn.addEventListener("click", () => {
-	nav.classList.toggle("navclose");
+    nav.classList.toggle("navclose");
+    if (isHidden) {
+        spans.forEach(span => {
+            span.style.display = 'inline'; // or 'inline' or their original display value
+        });
+        hideButton.textContent = 'Hide All'; // update the button label
+    } else {
+        spans.forEach(span => {
+            span.style.display = 'none';
+        });
+        hideButton.textContent = 'Show All'; // update the button label
+    }
+    isHidden = !isHidden; // toggle the state flag
 })
 

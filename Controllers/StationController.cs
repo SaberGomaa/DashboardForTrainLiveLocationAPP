06e4590441a0e2e3654c51d718021162f07d2d2d
sabegomaa261 <sabergomaa261@gmail.com>
2023-05-09 14:49:31 +0300

    Controllers

---
+++ Controllers/StationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dashboard.Controllers
+{
+    public class StationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

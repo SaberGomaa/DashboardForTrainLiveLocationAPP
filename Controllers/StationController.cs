1e43da238723098237a8a9d9e86fc105daa5f9ba
sabegomaa261 <sabergomaa261@gmail.com>
2023-05-18 05:44:17 +0300

    View All Stations

--- Controllers/StationController.cs
+++ Controllers/StationController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Test.Models;
 
 namespace Dashboard.Controllers
 {
     public class StationController : Controller
     {
-        public IActionResult Index()
+        HttpClient client = new HttpClient();
+        public StationController()
         {
-            return View();
+            client.BaseAddress = new Uri("http://saberelsayed-001-site1.itempurl.com/api/");
+        }
+
+        [HttpGet]
+        public IActionResult Show()
+        {
+
+            try
+            {
+                var result = client.GetAsync("station/getallstation").Result;
+                
+                var stations = result.Content.ReadAsAsync<IEnumerable<Station>>().Result;
+
+                return View(stations);
+            }
+            catch
+            {
+                return RedirectToAction("Error");
+            }
         }
     }
 }

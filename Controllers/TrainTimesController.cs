43ff007f98607219a9f0bc4e0dfa939a1ed28790
sabegomaa261 <sabergomaa261@gmail.com>
2023-06-24 18:21:37 +0300

    Add Train Times

--- Controllers/TrainTimesController.cs
+++ Controllers/TrainTimesController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Test.Models;
 
 namespace Dashboard.Controllers
 {
@@ -8,6 +10,29 @@ namespace Dashboard.Controllers
         public TrainTimesController()
         {
             client.BaseAddress = new Uri("http://trainlocationapi-001-site1.atempurl.com/api/");
+        }
+
+        public async Task<IActionResult> index()
+        {
+            var result = await client.GetAsync("Railway/GetAllRailways");
+            var Railways = await result.Content.ReadAsAsync<List<Railway>>();
+
+            SelectList railways = new SelectList(Railways, "Id", "Name");
+
+            ViewBag.Railways = railways;
+
+            return View();
+        }
+
+        public async Task<IActionResult> Add(int id)
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> View(int id)
+        {
+            return View();
         }
     }
 }

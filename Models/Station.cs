afa0c9aebbb33ec2ebafca503248c1212cd7c42c
sabegomaa261 <sabergomaa261@gmail.com>
2023-07-04 12:15:59 +0300

    Main Station

--- Models/Station.cs
+++ Models/Station.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Test.Models;
 
 namespace Test.Models
 {
@@ -7,25 +9,23 @@ namespace Test.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "Required")]
         public string? Name { get; set; }
-        [Required(ErrorMessage ="Required")]
+        [Required(ErrorMessage = "Required")]
         public string? NextStation { get; set; }
-        [Required(ErrorMessage ="Required")]
+        [Required(ErrorMessage = "Required")]
         public string? Description { get; set; }
-        [Required(ErrorMessage ="Required")]
+        [Required(ErrorMessage = "Required")]
         public int Position { get; set; }
-        [Required(ErrorMessage ="Required")]
+        [Required(ErrorMessage = "Required")]
         public double Longitude { get; set; }
-        [Required(ErrorMessage ="Required")]
+        [Required(ErrorMessage = "Required")]
         public double Latitude { get; set; }
-
-        [Required(ErrorMessage ="Required")]
+        [Required(ErrorMessage = "Required")]
         public int? NextStationPostion { get; set; }
+        [Required]
         public int? RailwayId { get; set; }
-
-        //[ForeignKey(nameof(Train))]
-        //public int TrainId { get; set; }
-        //public virtual Train? Train { get; set; }
+        [Required]
+        public bool MainStation { get; set; } = false;
         public ICollection<Ticket>? Tickets { get; set; }
 
-        //public ICollection<User>? Users { get; set; }
-    }}
+    }
+}
